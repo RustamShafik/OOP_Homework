374b35cd61d1ef8c237f6deef4fdafbc1d66cad6
Rustam Shafikov <mitaleko@gmail.com>
2025-02-13 13:15:04 +0500

    All done, formatting fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,8 @@ def category_one():
                 100000.50,
                 50,
             ),
-            Product("Siemens A50", "Очень старый телефон Siemens A50", 300, 10),
+            Product("Siemens A50", "Очень старый телефон Siemens A50",
+                    300, 10),
         ],
     )
 
