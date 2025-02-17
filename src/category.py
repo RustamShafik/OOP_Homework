855ef3af73d0915fc6f3ea0b7281f2320fba0ad8
Rustam Shafikov <mitaleko@gmail.com>
2025-02-17 16:17:34 +0500

    isinstance check added

--- src/category.py
+++ src/category.py
@@ -18,7 +18,8 @@ class Category:
         Category.category_count += 1
 
     def add_product(self, product: Product):
-        self.__products.append(product)
+        if isinstance(product, Product):
+            self.__products.append(product)
 
     @property
     def products(self):
